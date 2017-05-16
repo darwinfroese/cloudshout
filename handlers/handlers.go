2bf1246c0ed9f8953872149637251010bf80e540
Darwin Froese <froese.darwin@gmail.com>
2017-05-15 20:26:12 -0500

    pushing minor changes

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -10,14 +10,24 @@ type index struct {
 	Title string
 }
 
+type admin struct {
+	Title     string
+	Heading   string
+	Templates []string
+}
+
+type theme struct {
+	Name            string
+	FontColor       string
+	BackgroundColor string
+}
+
 // RenderIndex - Will be used to render our index.html web page
 func RenderIndex(w http.ResponseWriter, r *http.Request) {
 	i := index{Title: "Cloudshout Official Blog"}
 
-	fmt.Println("Proto: ", r.Proto)
-
 	if pusher, ok := w.(http.Pusher); ok {
-		if err := pusher.Push("web/main.css", nil); err != nil {
+		if err := pusher.Push("/main.css", nil); err != nil {
 			fmt.Println("Error:", err.Error())
 		}
 	} else {
@@ -35,4 +45,42 @@ func RenderIndex(w http.ResponseWriter, r *http.Request) {
 	}
 
 	t.Execute(w, i)
+}
+
+// RenderCSS - Serves main.css
+func RenderCSS(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "web/main.css")
+}
+
+// RenderAdmin - Will render the admin page with templated options
+func RenderAdmin(w http.ResponseWriter, r *http.Request) {
+	a := admin{Title: "Cloudshout Official Blog - Admin", Heading: "Admin Page"}
+	a.Templates = []string{"Text Post", "Half Width Text Post"}
+
+	if pusher, ok := w.(http.Pusher); ok {
+		if err := pusher.Push("/main.css", nil); err != nil {
+			fmt.Println("Error:", err.Error())
+		}
+	} else {
+		fmt.Println("Couldn't push main.css")
+	}
+
+	t, err := template.ParseFiles("web/admin.html")
+
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+
+		e := http.StatusInternalServerError
+		http.Error(w, http.StatusText(e), e)
+		return
+	}
+
+	t.Execute(w, a)
+}
+
+// CreatePostHandler - Creates a blog post
+func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		// Create Blog Post
+	}
 }
--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	m := mux.NewMux()
 
 	m.AddRoute("/", handlers.RenderIndex)
+	m.AddRoute("/main.css", handlers.RenderCSS)
+	m.AddRoute("/admin", handlers.RenderAdmin)
+	m.AddRoute("/api/v1/blog", handlers.CreatePostHandler)
 
 	log.Fatal(http.ListenAndServeTLS(":8080", "server.crt", "server.key", m))
 }
